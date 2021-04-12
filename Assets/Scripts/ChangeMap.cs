36ee5443c4666c904f1897d045bc3cfefef39e9c
Adria Sebastia <adriguiu97@gmail.com>
2021-04-12 14:06:55 +0200

    Light/Dark Map

---
+++ Assets/Scripts/ChangeMap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMap : MonoBehaviour
+{
+    [SerializeField] GameObject lightMap;
+    //[SerializeField] Animator animator;
+    [SerializeField] GameObject darkMap;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (lightMap.activeInHierarchy)
+            {
+                //animator.SetBool("ChangeToDark", true);
+                lightMap.SetActive(false);
+                darkMap.SetActive(true);
+            } else if (darkMap.activeInHierarchy)
+            {
+                //animator.SetBool("ChangeToDark", false);
+                lightMap.SetActive(true);
+                darkMap.SetActive(false);
+            }
+        }
+    }
+}
