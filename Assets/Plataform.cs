0d1d2dbe127696eb299a42a0da9b571fa5e25b57
Adria Sebastia <adriguiu97@gmail.com>
2021-04-13 09:14:14 +0200

    Movimento Plataforma Fixed

--- Assets/Plataform.cs
+++ Assets/Plataform.cs
@@ -6,19 +6,40 @@ public class Plataform : MonoBehaviour
 {
     public float Speed;
     Rigidbody2D Rigidbody2D;
-    private float Speed_;
+    public Transform target;
+
+    private Transform startPos;
+    private bool move;
 
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        startPos = transform;
+        move = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rigidbody2D.velocity = new Vector2(Speed, Rigidbody2D.velocity.y);
+        float step = Speed * Time.deltaTime;
+        if (transform.position == target.position)
+        {
+            move = false;
+        }
+        else if (transform == startPos)
+        {
+            move = true;
+        }
+        if (move == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos.position, step);
+        }
+        else if (move)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,8 +47,8 @@ public class Plataform : MonoBehaviour
         if (collision.gameObject.tag == "Border")
         {
             Speed *= -1;
-            
+
         }
     }
-    
+   
 }
---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    [SerializeField] float direction;
+    [SerializeField] bool horizontal;
+    [SerializeField] bool vertical;
+
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        if (horizontal)
+        {
+            rigidbody.velocity = new Vector2(direction * speed, rigidbody.velocity.y);
+        }
+        else if (vertical)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, direction * speed);
+        }
+    }
+}
